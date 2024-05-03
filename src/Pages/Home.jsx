6cb7045fea8dfdbc06ef2690ea0e55c6fe50602f
Ssewagudde David <dsewagude10@gmail.com>
2024-05-04 00:07:36 +0300

    dashboard

--- src/Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Home = () => {
-  return (
-    <div className="text-xl font-bold text-red-600">Welcome to the Home Page</div>
-  )
-}
-
-export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import Home from './Pages/Home';
+import "./index.css";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import HomePage from "./Pages/HomePage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />
-  },
-  {
-    path: "/signup",
-    element: <Signup />
-  },
-  {
-    path: "/home",
-    element: <Home />
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <Signup />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Home />,
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
