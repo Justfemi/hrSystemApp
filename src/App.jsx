b4a5737d779bc10cae8c855576d017d8b3b7c433
AJIBADE Olufemi <62211699+Justfemi@users.noreply.github.com>
2024-05-15 03:02:47 +0100

    Revert "modified authpages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// import HomePage from "./Pages/HomePage";
+import React from "react";
+import HomePage from "./Pages/HomePage";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
--- src/components/ActiveCards.jsx
+++ src/components/ActiveCards.jsx
@@ -1,21 +1,19 @@
+import React from "react";
 import Card from "./Card";
 import { User } from "lucide-react";
 
 const cardData = [
   {
-    id: 1,
     title: "Number of Employees",
     count: 150, // Example value
     icon: "employee", // You can use icons if needed
   },
   {
-    id: 2,
     title: "Number of Leaves",
     count: 25, // Example value
     icon: "leave", // You can use icons if needed
   },
   {
-    id: 3,
     title: "Number of Projects",
     count: 10, // Example value
     icon: "project", // You can use icons if needed
@@ -26,8 +24,8 @@ const cardData = [
 const ActiveCards = () => {
   return (
     <div className="bg-red-100 grid grid-cols-3 gap-10 py-10 px-5">
-      {cardData.map((card, key) => (
-        <Card key={key.id}>
+      {cardData.map((card) => (
+        <Card>
           <div className="flex space-x-4">
             <User className="h-12 bg-red-400 px-5 rounded-full" />
             <div>
