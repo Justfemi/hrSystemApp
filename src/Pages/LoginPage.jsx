41d6e3ff675d9ed3cd39aacca00ac0355ebb5200
Justfemi <ajibade.olufemi@yahoo.com>
2024-05-15 02:59:22 +0100

    bug:installed packages

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -21,12 +21,10 @@ const LoginPage = () => {
           <div className="flex flex-col">
             <label className="mb-1">Email</label>
             <input type="email" className="w-full px-3 py-2 leading-tight border rounded-md shadow-sm focus:outline-none focus:border-red-400"  />
-            {/* error */}
           </div>
           <div className="flex flex-col my-3">
             <label className="mb-1">Password</label>
             <input type="password" className="w-full px-3 py-2 leading-tight border rounded-md shadow-sm focus:outline-none focus:border-red-400" />
-            {/* error */}
           </div>
 
           <p>
