4e6a44771ee1004707fda6ca9424a5051a9d0064
Justfemi <ajibade.olufemi@yahoo.com>
2024-05-22 00:46:44 +0100

    ch:modified user dashboard

--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import userImg from '../assets/user-profile.png';
 
 const UserDashboard = () => {
   const { user, logout } = useContext(AuthContext);
-
 
   // State to manage edit mode
   const [isEditing, setIsEditing] = useState(false);
@@ -39,69 +39,26 @@ const UserDashboard = () => {
         </div>
       </header>
 
-      <main className="  bg-red-500 w-full">
-        <div className="h-[100vh] p-6 grid grid-cols-3 gap-5">
+      <main className="  bg-red-500 w-full h-full">
+        <div className="py-10">
           <form
             onSubmit={handleSubmit}
-            className="flex flex-col items-center bg-gray-200 rounded-lg  col-span-2 "
+            className="flex flex-col items-center bg-gray-200 rounded-lg col-span-2 mx-auto w-[60%] py-10"
           >
-            <h2>user profile</h2>
+            {/* <h2>user profile</h2>
             <h2>{user.name}</h2>
-            <h2>{user.id}</h2>
-            <div className="flex space-x-4">
-              <div className="mb-4">
-                <label className="block text-gray-700 font-bold mb-2">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  value={user.name}
-                  onChange={handleChange}
-                  disabled={!isEditing}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 font-bold mb-2">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  value={user.name}
-                  onChange={handleChange}
-                  disabled={!isEditing}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-              </div>
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={user.name}
-                onChange={handleChange}
-                disabled={!isEditing}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <h2>{user.id}</h2> */}
+            <div className="mb-3">
+              <img 
+                src={userImg} 
+                alt="user-profile"
+                className="w-[120px]"
               />
             </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={user.name}
-                onChange={handleChange}
-                disabled={!isEditing}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div>
+            <div className="flex space-x-8 mb-3">
               <div className="mb-4">
                 <label className="block text-gray-700 font-bold mb-2">
-                  Name
+                  Full Name
                 </label>
                 <input
                   type="text"
@@ -114,27 +71,28 @@ const UserDashboard = () => {
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 font-bold mb-2">
-                  Name
+                  Email
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  value={user.name}
+                  name="email"
+                  value={user.email}
                   onChange={handleChange}
                   disabled={!isEditing}
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 />
               </div>
             </div>
-            <div className="flex">
+
+            <div className="flex space-x-8 mb-3">
               <div className="mb-4">
                 <label className="block text-gray-700 font-bold mb-2">
-                  Name
+                  Gender
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  value={user.name}
+                  name="gender"
+                  value={user.gender}
                   onChange={handleChange}
                   disabled={!isEditing}
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -142,29 +100,77 @@ const UserDashboard = () => {
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 font-bold mb-2">
-                  Name
+                  Contact
                 </label>
                 <input
                   type="text"
-                  name="name"
-                  value={user.name}
+                  name="contact"
+                  value={user.contact}
                   onChange={handleChange}
                   disabled={!isEditing}
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 />
               </div>
             </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={user.name}
-                onChange={handleChange}
-                disabled={!isEditing}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
+
+            <div className="flex space-x-8 mb-3">
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">
+                  Role
+                </label>
+                <input
+                  type="text"
+                  name="role"
+                  value={user.role}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">
+                  Date of birth
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={user.date_of_birth}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
             </div>
+
+            <div className="flex space-x-8 mb-3">
+              <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">
+                  Password
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  value={user.password}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              {/* <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">
+                  Date of birth
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={user.date_of_birth}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div> */}
+            </div>
+
             {/* Other input fields go here */}
             <div className="flex items-center justify-between">
               <button
@@ -185,21 +191,22 @@ const UserDashboard = () => {
                 Save
               </button>
             </div>
+
+            <div className="mt-10">
+              <Link
+                to={`/apply-leave`}
+                className="m-4 p-3 bg-gray-900 text-white rounded"
+              >
+                Apply for Leave
+              </Link>
+              <Link
+                to={`/attendance`}
+                className="m-4 p-3 bg-gray-900 text-white rounded"
+              >
+                Check Attendance
+              </Link>
+            </div>
           </form>
-          <div>
-            <Link
-              to={`/apply-leave`}
-              className="m-4 p-4 bg-gray-900 text-white rounded"
-            >
-              Apply for Leave
-            </Link>
-            <Link
-              to={`/attendance`}
-              className="m-4 p-4 bg-gray-900 text-white rounded"
-            >
-              Check Attendance
-            </Link>
-          </div>
         </div>
       </main>
     </div>
